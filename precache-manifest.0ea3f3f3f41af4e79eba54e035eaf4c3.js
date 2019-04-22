607edb284d27b70798677a0fdebfa3abcd0d095a
mrc <marcoambrosini@pm.me>
2019-04-22 16:59:00 +0200

    Updates

---
+++ precache-manifest.0ea3f3f3f41af4e79eba54e035eaf4c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b958f8aeb249dad7fe7",
+    "url": "/pomodoro-timer/static/css/main.10f2000f.chunk.css"
+  },
+  {
+    "revision": "1b958f8aeb249dad7fe7",
+    "url": "/pomodoro-timer/static/js/main.e4a90729.chunk.js"
+  },
+  {
+    "revision": "8d84c672a99e0f0ece83",
+    "url": "/pomodoro-timer/static/js/runtime~main.2a0df6ba.js"
+  },
+  {
+    "revision": "a9a9ee801bbe191bb794",
+    "url": "/pomodoro-timer/static/js/2.fae8e409.chunk.js"
+  },
+  {
+    "revision": "15e36ae1087e48402fc7b642665633ce",
+    "url": "/pomodoro-timer/index.html"
+  }
+];
